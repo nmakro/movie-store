aacc6b4d78139d8f69365246445b42d4188f2548
Nikos Makropoulos <nmakro@gmail.com>
2020-07-10 02:26:49 +0300

    Create app

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from .config import configMap
+
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app_config = configMap[os.getenv("ENV_TYPE", default="PROD_ENV")]
+
+    app.config.from_object(app_config)
+    os.makedirs(app.instance_path, exist_ok=True)
+
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    from .api import bp
+
+    app.register_blueprint(bp, url_prefix="/api/v1")
+
+    return app
