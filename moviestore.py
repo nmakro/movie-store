9216fed748901ce03bccb3619d35763a2cce9460
Nikos Makropoulos <nmakro@gmail.com>
2020-07-11 22:36:12 +0300

    Add flask shell-context

--- moviestore.py
+++ moviestore.py
@@ -1,6 +1,16 @@
 from app import create_app
+from app import db
+from app.model.categories import Category
+from app.model.movies import Movie
+from app.model.users import User
 
 app = create_app()
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Movie': Movie, 'Category': Category}
+
 
 if __name__ == "__main__":
     app.run()
