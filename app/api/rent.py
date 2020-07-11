07bdc0e1bbe36d3208d490f35a0e2cc3ebb1685e
Nikos Makropoulos <nmakro@gmail.com>
2020-07-12 01:23:05 +0300

    Add functionality for rent endpoint

--- app/api/rent.py
+++ app/api/rent.py
@@ -1,7 +1,38 @@
+from flask import request, jsonify
 from app.api import bp
+from app import db
+from app.model.movies import Movie
+from app.model.users import User
 
 
-@bp.route("/rent/<int:title-id>", methods=["POST"])
-def rent_title():
-    pass
+@bp.route("/rent/<string:username>", methods=["POST"])
+def rent_title(username):
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        payload = {"error": "User not found"}
+        res = jsonify(payload)
+        res.status_code = 400
+        return res
+    movie_id = request.args.get("movie_id")
+    movie_name = request.args.get("movie_name")
+    if movie_id is None and movie_name is None:
+        payload, status_code = {"error": "Please provide either the movie_id or movie_name for the movie you want to rent!"}, 404
+        res = jsonify(payload)
+        res.status_code = 400
+        return res
+    elif movie_name is not None:
+        movie = Movie.query.filter_by(title=movie_name).first()
+        if not movie:
+            payload = {"error": "Movie not found"}
+            res = jsonify(payload)
+            res.status_code = 404
+            return res
+        user.rent_movie(movie)
+        db.session.add(user)
+        db.session.commit()
+        res = jsonify({})
+        res.status_code = 201
+        return res
+
+
 
--- app/api/users.py
+++ app/api/users.py
@@ -4,18 +4,38 @@ from app.model.users import User
 
 
 @bp.route("/users/<int:user_id>", methods=["GET"])
-def get_user(user_id):
-    return jsonify(User.query.get_or_404(user_id).user_dict())
+def get_user_by_id(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    payload, status_code = (user.user_dict(), 200) if user else ({"error": "User not found"}, 404)
+    res = jsonify(payload)
+    res.status_code = status_code
+    return res
 
 
+@bp.route("/users/<string:username>", methods=["GET"])
+def get_user_by_name(username):
+    user = User.query.filter_by(username=username).first()
+    payload, status_code = (user.user_dict(), 200) if user else ({"error": "User not found"}, 404)
+    res = jsonify(payload)
+    res.status_code = status_code
+    return res
+
+
+@bp.route("/users/", methods=["GET"])
 @bp.route("/users", methods=["GET"])
 def get_users():
-    res = User.query.order_by(User.id.desc()).paginate(1, 20, False).items
-    data = {"items": [user.user_dict() for user in res]}
-    return jsonify(data)
+    if request.args.get("username"):
+        user = User.query.filter_by(username=request.args.get("username")).first()
+        payload, status_code = (user.user_dict(), 200) if user else ({"error": "User not found"}, 404)
+    else:
+        res = User.query.order_by(User.username.desc()).paginate(1, 20, False).items
+        payload, status_code = ({"items": [user.user_dict() for user in res]}, 200) if res else ({"error": "No users found!"}, 404)
+    res = jsonify(payload)
+    res.status_code = status_code
+    return res
 
 
-@bp.route("/users/<int:user_id>/movies", methods=["GET"])
+@bp.route("/users/<string:username>/history", methods=["GET"])
 def user_movies(user_id):
-    return jsonify(User.query.get_or_404(user_id).get_movies())
+    pass
 
