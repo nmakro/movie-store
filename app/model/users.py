f7bb86f3c5ed1d3f6ecb6ab5fa427886e9e163c4
Nikos Makropoulos <nmakro@gmail.com>
2020-07-11 22:38:13 +0300

    Remove id from response

--- app/model/users.py
+++ app/model/users.py
@@ -1,4 +1,3 @@
-import json
 from app import db
 from app.model.table_associations import UserMovies
 
@@ -11,7 +10,6 @@ class User(db.Model):
 
     def user_dict(self):
         data = {
-            "id": self.id,
             "username": self.username,
             "movies": [m.title for m in self.movies]
         }
@@ -24,5 +22,4 @@ class User(db.Model):
         return data
 
     def __repr__(self):
-        return f"User {self.username}, user id: {self.id}"
-
+        return f"User: {self.username}"
