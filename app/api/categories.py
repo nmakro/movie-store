c8a7f264aed1338690a904b585a35b5448c308ed
Nikos Makropoulos <nmakro@gmail.com>
2020-07-11 22:43:01 +0300

    Fix status codes

--- app/api/categories.py
+++ app/api/categories.py
@@ -11,15 +11,15 @@ def get_categories():
         if genre:
             if genre == "all":
                 items = Category.query.order_by(Category.genre.desc()).paginate(1, 20, False).items
-                data, status_code = ({"items": [c.category_dict() for c in items]}, 200) if items else ({"error:": "No categories found"}, 400)
+                data, status_code = ({"items": [c.category_dict() for c in items]}, 200) if items else ({"error:": "No categories found"}, 404)
             else:
                 c = Category.query.filter_by(genre=genre).first()
-                data, status_code = ({"category": c.genre, "movies": [m.title for m in c.movies]}, 200) if c is not None else ({"movies": []}, 400)
+                data, status_code = ({"category": c.genre, "movies": [m.title for m in c.movies]}, 200) if c is not None else ({"movies": []}, 404)
         else:
-            data, status_code = ({"error": "Unknown query param"}, 404)
+            data, status_code = ({"error": "Unknown query param"}, 400)
     else:
         items = Category.query.order_by(Category.genre.desc()).paginate(1, 20, False).items
-        data, status_code = ({"items": [c.genre for c in items]}, 200) if items else ({"error:": "No categories found"}, 400)
+        data, status_code = ({"items": [c.genre for c in items]}, 200) if items else ({"error:": "No categories found"}, 404)
     res = jsonify(data)
     res.status_code = status_code
     return res
