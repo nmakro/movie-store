0154641003e8bdc59c08fae3a30b05ee528caf66
Nikos Makropoulos <nmakro@gmail.com>
2020-07-14 03:30:22 +0300

    Add authorization

---
+++ app/api/auth.py
@@ -0,0 +1,23 @@
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
+auth = HTTPBasicAuth()
+
+users = {
+    "admin": generate_password_hash("admin_pass"),
+    "Chris": generate_password_hash("test_user_pass")
+}
+
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and check_password_hash(users.get(username), password):
+        return username
+
+
+@auth.get_user_roles
+def get_user_roles(user):
+    if user == "admin":
+        return "administrator"
+    if user == "Chris":
+        return "test"
