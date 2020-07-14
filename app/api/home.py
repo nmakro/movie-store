a40e123afeb7a234322effdc9e928937014f1192
Nikos Makropoulos <nmakro@gmail.com>
2020-07-14 03:31:07 +0300

    Add home page for user

---
+++ app/api/home.py
@@ -0,0 +1,18 @@
+from flask import request, jsonify
+from app.api import bp
+from app.api.errors import not_found_response, unauthorized_acess, already_exists_response, bad_request_response, successful_update
+from app.model.users import User
+from app.api.auth import auth
+
+
+@bp.route("/home/")
+@bp.route("/home")
+@auth.login_required
+def get_home_page():
+    user = User.query.filter_by(username=auth.current_user()).first()
+    if not (auth.current_user() == "admin" or auth.username() == user.username):
+        return unauthorized_acess()
+    payload, status_code = user.user_dict(username=False), 200
+    res = jsonify(payload)
+    res.status_code = status_code
+    return res
